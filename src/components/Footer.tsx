5bd19e756cb3b4f1116c6ca8a80b2b2157eaf988
Nick2858 <nicholaskarantakis@gmail.com>
2024-07-21 04:10:02 -0400

    modified:   index.html 	new file:   public/In-White-128.png 	new file:   public/LetterN.png 	deleted:    public/diverterSign.png 	new file:   public/github-mark-white.svg 	deleted:    public/vite.svg 	modified:   src/App.css 	deleted:    src/assets/diverterSign.png 	modified:   src/components/Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,19 @@
 import "../App.css";
 
 function Footer() {
-  return <div className="Footer"></div>;
+  return (
+    <div className="Footer">
+      <a href="https://github.com/Nick2858" target="_blank">
+        <img className="Icon" src="/github-mark-white.svg"></img>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/nicholas-karantakis-78a54a255"
+        target="_blank"
+      >
+        <img className="Icon" src="/In-White-128.png"></img>
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
