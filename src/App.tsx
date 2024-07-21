e3050143bb5afd7e17f2786245ca485d6e927b53
Nick2858 <nicholaskarantakis@gmail.com>
2024-07-21 02:33:39 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
 import LandingPage from "./components/LandingPage";
-import NavBar from "./components/NavBar";
 import AboutPage from "./components/AboutPage";
 import ProjectsPage from "./components/ProjectsPage";
+import {
+  Link,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from "react-scroll";
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <LandingPage />
-      <AboutPage />
-      <ProjectsPage />
+      <div className="NavBar">
+        <Link className="NavPages" to="Home">
+          {" "}
+          Home{" "}
+        </Link>
+        <span className="NavText"> | </span>
+        <Link className="NavPages" to="About">
+          {" "}
+          About{" "}
+        </Link>
+        <span className="NavText"> | </span>
+        <Link className="NavPages" to="Projects">
+          {" "}
+          Projects{" "}
+        </Link>
+      </div>
+      <Element name="Home">
+        <LandingPage />
+      </Element>
+      <Element name="About">
+        <AboutPage />
+      </Element>
+      <Element name="Projects">
+        <ProjectsPage />
+      </Element>
     </div>
   );
 }
