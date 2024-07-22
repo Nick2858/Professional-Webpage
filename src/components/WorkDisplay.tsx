c203802ac6498a274a4fc5992698bbf014f821cf
Nick2858 <nicholaskarantakis@gmail.com>
2024-07-22 09:17:05 -0400

    modified:   src/App.css 	modified:   src/components/WorkDisplay.tsx

--- src/components/WorkDisplay.tsx
+++ src/components/WorkDisplay.tsx
@@ -15,7 +15,18 @@ function WorkDisplay({ children }: Props) {
   return (
     <div className="WorkDisplay">
       <img src={children.image} className="WorkImage"></img>
-      <div className="WorkInfo">{children.jobtitle}</div>
+      <div className="WorkInfo">
+        <h1 style={{ fontSize: "20px" }}>
+          {children.jobtitle} - {children.company} <br></br>
+        </h1>
+        <h1 style={{ fontSize: "15px" }}>{children.date}</h1>
+        <h1 style={{ fontSize: "18px", fontWeight: "600", paddingTop: "5px" }}>
+          Role:
+        </h1>
+        <b style={{ fontSize: "15px", fontWeight: "400" }}>
+          {children.description}
+        </b>
+      </div>
     </div>
   );
 }
