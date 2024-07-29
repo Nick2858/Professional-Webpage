83b147134ec8f899d5d1bb11756ba49964e4d32d
Nick2858 <nicholaskarantakis@gmail.com>
2024-07-29 15:47:35 -0400

    modified:   src/App.css 	modified:   src/components/ProjectsPage.tsx

--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -11,13 +11,15 @@ function ProjectsPage() {
       <h1 className="PageTitle" style={{ fontSize: "80px" }}>
         Projects
       </h1>
-      <Canvas className="canvas">
-        <ambientLight />
-        <Suspense fallback={null}>
-          <Model scale={0.015} />
-          <OrbitControls enableZoom={false} />
-        </Suspense>
-      </Canvas>
+      <div className="canvas">
+        <Canvas>
+          <ambientLight />
+          <Suspense fallback={null}>
+            <Model scale={0.015} />
+            <OrbitControls enableZoom={false} />
+          </Suspense>
+        </Canvas>
+      </div>
     </div>
   );
 }
