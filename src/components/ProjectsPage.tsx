9d3e9811b6104d2c6b953e98288b926eb33fd21f
Nick2858 <nicholaskarantakis@gmail.com>
2024-07-29 15:37:49 -0400

    new file:   FullDemoGen.jsx 	modified:   package-lock.json 	modified:   package.json 	new file:   public/FullDemoGenDeci.glb 	modified:   src/App.css 	new file:   src/components/3DModel.tsx 	new file:   src/components/FullDemoGenDeci.tsx 	modified:   src/components/ProjectsPage.tsx

--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -1,7 +1,25 @@
 import "../App.css";
+import Model from "./FullDemoGenDeci";
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
 
 function ProjectsPage() {
-  return <div className="ProjectsPage">Hello</div>;
+  return (
+    <div className="ProjectsPage">
+      <h1 className="PageTitle" style={{ fontSize: "80px" }}>
+        Projects
+      </h1>
+      <Canvas className="canvas">
+        <ambientLight />
+        <Suspense fallback={null}>
+          <Model scale={0.5} />
+          <OrbitControls enableZoom={true} />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
 
 export default ProjectsPage;
