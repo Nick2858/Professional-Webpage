3ba602260ff221d7eda5c34fccf0853739a19072
Nick2858 <nicholaskarantakis@gmail.com>
2024-11-23 01:50:16 -0500

    updated menu bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,19 +7,42 @@ import {
   scroller,
 } from "react-scroll";
 import { NavLink as Link } from "react-router-dom";
+import { useState } from "react";
 
-const NavBar = () => {
+function NavBar() {
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownVisible(!isDropdownVisible);
+  };
+
   return (
-    <nav className="NavBar">
-      <Link className="NavPages" to="/Portfolio-Website/">
-        <img className="HomeButton" src="Home.png"></img>
-      </Link>
-
-      <Link className="NavPages" to="/Portfolio-Website/Projects">
-        Projects
-      </Link>
-    </nav>
+    <>
+      <nav className="NavBar">
+        {isDropdownVisible && (
+          <>
+            <Link className="NavPages" to="/Portfolio-Website/">
+              Home
+            </Link>
+            <Link className="NavPages" to="/Portfolio-Website/Projects">
+              Projects
+            </Link>
+            <Link className="NavPages" to="/Portfolio-Website/Projects">
+              Experiences
+            </Link>
+          </>
+        )}
+        <button
+          className={!isDropdownVisible ? "Menu" : "change"}
+          onClick={toggleDropdown}
+        >
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
+        </button>
+      </nav>
+    </>
   );
-};
+}
 
 export default NavBar;
