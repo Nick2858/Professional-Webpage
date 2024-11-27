cefdf6364641dba8e7734917bca511e399c92993
Nick2858 <nicholaskarantakis@gmail.com>
2024-11-27 01:50:23 -0500

    Update landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -2,7 +2,9 @@ import "../App.css";
 import { Link } from "react-scroll";
 import { useState, useEffect } from "react";
 
+
 function LandingPage() {
+  
   const [isVisible, setIsVisible] = useState(true);
   useEffect(() => {
     const handleScroll = () => {
@@ -23,53 +25,51 @@ function LandingPage() {
       <div id="Home" className="LandingPage">
         {isVisible && (
           <>
-            <div className="Name"> Nicholas Karantakis </div>
-
-            <div className="InnerPage">
-              <img className="Portrait" src="Portrait.png"></img>
-              <div className="Socials">
+        <div className="Name"> Nicholas Karantakis </div>
+      
+        <div className="InnerPage">
+          <img className="Portrait" src="Portrait.png"></img>
+          <div className="Socials">
                 <div className="IconsGroup">
-                  <a href="https://github.com/Nick2858" target="_blank">
+              <a href="https://github.com/Nick2858" target="_blank">
                     <img
                       className="IconLink"
                       src="./github-mark-white.svg"
                     ></img>
-                  </a>
-                  <a
-                    href="https://www.linkedin.com/in/nicholas-karantakis-78a54a255"
-                    target="_blank"
-                  >
-                    <img className="IconLink" src="./In-White-128.png"></img>
-                  </a>
-                  <a href="mailto:nicholaskarantakis@gmail.com" target="_blank">
-                    <img className="IconLink" src="./gmail.svg"></img>
-                  </a>
-                  <div className="ResumeLink">
-                    <a href="./Resume.pdf" target="_blank">
-                      <button className="ResumeLinkText">Resume</button>
-                    </a>
-                  </div>
-                </div>
-              </div>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/nicholas-karantakis-78a54a255"
+                target="_blank"
+              >
+                <img className="IconLink" src="./In-White-128.png"></img>
+              </a>
+              <a href="mailto:nicholaskarantakis@gmail.com" target="_blank">
+                <img className="IconLink" src="./gmail.svg"></img>
+              </a>
+            <div className="ResumeLink">
+            <a href="./Resume.pdf" target="_blank">
+              <button className="ResumeLinkText">Resume</button>
+            </a>
+            </div>
+          </div>
+       </div>
             </div>
           </>
         )}
       </div>
       <div className="ExploreBar">
-        {isVisible && (
-          <>
-            <Link to="AboutPage">
-              <div className="Explore"> Press to Explore </div>
-              <input
-                className="Arrow"
-                type="image"
-                src="./planeArrow.png"
-              ></input>
-            </Link>
-          </>
-        )}
-      </div>
+        
+        {isVisible &&( 
+        <>
+        <Link to="AboutPage">
+          <div className="Explore"> Press to Explore </div>
+          <input className="Arrow" type="image" src="./planeArrow.png"></input>
+          
+        </Link></>)}
+      </div>  
+      
     </>
+  
   );
 }
 
